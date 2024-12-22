9ab02cf8b17c47aecaa5a267509489b8a5d0fa5b
Arsenii Piatko <arseniipiatko@gmail.com>
2024-12-22 12:53:23 +0100

    Change to the mechanics

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
+//import state functions
+import {
+  declareWinner,
+  endTheGame,
+  fight,
+  nextStatus,
+  readyToStartUpdate,
+} from "./stateFunctions.js";
+
 // Define the game states using an enum-like object
-const GameState = Object.freeze({
+export const GameState = Object.freeze({
   READY_TO_START: "READY_TO_START",
-  PLAYING: "PLAYING",
+  PASSED: "PASSED",
+  ENCOUTERED: "ENCOUTERED",
   LOST: "LOST",
   WON: "WON",
+  END_OF_GAME: "END_OF_GAME",
 });
 
 const payoutsAmounts = { 1: 1000000000 };
@@ -20,17 +31,15 @@ for (let i = 1; i <= 100; i++) {
   payoutsAmounts[i] = formatCurrency(payoutsAmounts[i]);
 }
 
-function randBetween(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 // Initialize the game state to READY_TO_START
 let currentState = GameState.READY_TO_START;
 
 // Initialize game variables
-let currentPosition = 100;
-let currentPayout = payoutsAmounts[currentPosition];
-let currentLeave = randBetween(1, 100);
+let players = {};
+let currentPosition = {};
+let currentPayout = {};
+let currentPair = [undefined, undefined];
+let currentSkill = 0.5;
 
 // Get references to buttons, status, and playing info
 const restartButton = document.getElementById("restart");
@@ -39,10 +48,39 @@ const statusText = document.querySelector("#status p");
 const playingInfoDiv = document.getElementById("playing-info");
 const currentPositionElement = document.getElementById("current-position");
 const currentPayoutElement = document.getElementById("current-payout");
-const skillFactor = document.getElementById("skill-factor");
-const inexpFactor = document.getElementById("inexperience-factor");
 const userInput = document.getElementById("user-input");
 const inputSection = document.getElementById("input-section");
+const playerGrid = document.querySelector(".player-grid");
+
+function updateGrid() {
+  const existingItems = playerGrid.querySelectorAll(".grid-item");
+  existingItems.forEach((item) => item.remove());
+
+  // Add data rows
+  Object.keys(players).forEach((player) => {
+    const playerCell = document.createElement("div");
+    playerCell.className = "grid-item";
+    playerCell.textContent = `Player ${players[player].id}`;
+
+    const killsCell = document.createElement("div");
+    killsCell.className = "grid-item";
+    killsCell.textContent = players[player].kills;
+
+    const positionCell = document.createElement("div");
+    positionCell.className = "grid-item";
+    positionCell.textContent = players[player].position;
+
+    const payoutCell = document.createElement("div");
+    payoutCell.className = "grid-item";
+    payoutCell.textContent = players[player].payout;
+
+    // Append cells to the grid
+    playerGrid.appendChild(playerCell);
+    playerGrid.appendChild(killsCell);
+    playerGrid.appendChild(positionCell);
+    playerGrid.appendChild(payoutCell);
+  });
+}
 
 // Update the game status and button text based on the current state
 function updateGameStatus() {
@@ -55,68 +93,122 @@ function updateGameStatus() {
       playingInfoDiv.classList.add("hidden"); // Hide the playing info
       statusText.classList.remove("p-no-border"); // Remove larger font and border
       inputSection.classList.remove("hidden");
+      playerGrid.classList.add("hidden");
+      readyToStartUpdate(
+        players,
+        currentPosition,
+        currentPayout,
+        payoutsAmounts,
+        currentPair
+      );
       break;
-    case GameState.PLAYING:
-      statusText.textContent = "Game is in progress...";
+    case GameState.PASSED:
+      statusText.textContent = `Player ${currentPair[0]} have encoutered Player ${currentPair[1]}`;
       restartButton.disabled = false; // Enable restart button
       startButton.disabled = false; // Enable start button
-      startButton.textContent = "Next"; // Change text to "Next"
+      startButton.textContent = "Continue"; // Change text to "Next"
       playingInfoDiv.classList.remove("hidden"); // Show the playing info
       statusText.classList.add("p-no-border"); // Apply larger font and remove border
       inputSection.classList.add("hidden");
+      playerGrid.classList.remove("hidden");
+      updatePositionAndPayout(); // Update position and payout
+      break;
+    case GameState.ENCOUTERED:
+      statusText.textContent = `You have encoutered Player ${
+        currentPair[0] + currentPair[1] - 1
+      }`;
+      restartButton.disabled = false; // Enable restart button
+      startButton.disabled = false; // Enable start button
+      startButton.textContent = "Fight"; // Change text to "Next"
+      playingInfoDiv.classList.remove("hidden"); // Show the playing info
+      statusText.classList.add("p-no-border"); // Apply larger font and remove border
+      inputSection.classList.add("hidden");
+      playerGrid.classList.remove("hidden");
+      startButton.style.backgroundColor = "red";
       updatePositionAndPayout(); // Update position and payout
       break;
     case GameState.LOST:
       statusText.textContent = "You lost the game!";
-      restartButton.disabled = false; // Enable restart button
-      startButton.disabled = true; // Disable start button
-      startButton.textContent = "Next"; // Change text to "Next"
-      playingInfoDiv.classList.remove("hidden"); // Show the playing info
-      statusText.classList.add("p-no-border"); // Apply larger font and remove border
-      inputSection.classList.add("hidden");
+      startButton.textContent = "Results"; // Change text to "Next"
+      startButton.style.backgroundColor = "white";
       updatePositionAndPayout(); // Update position and payout
       break;
     case GameState.WON:
-      statusText.textContent = "You won the game!";
-      startButton.disabled = true; // Disable start button
+      statusText.textContent = "You won the battle!";
+      startButton.textContent = "Continue";
+      startButton.style.backgroundColor = "white";
       updatePositionAndPayout();
+      break;
+    case GameState.END_OF_GAME:
+      statusText.textContent = declareWinner(players, payoutsAmounts);
+      startButton.disabled = true; // Enable start button
+      playingInfoDiv.classList.add("hidden");
+      updatePositionAndPayout(); // Update position and payout
       break;
   }
 }
 
 // Function to update position and payout
 function updatePositionAndPayout() {
-  currentPositionElement.textContent = currentPosition;
-  currentPayoutElement.textContent = currentPayout;
+  currentPositionElement.textContent = currentPosition["current"];
+  currentPayoutElement.textContent = currentPayout["current"];
+  updateGrid();
 }
 
 // Start button functionality (Now also handles "Next" behavior)
 startButton.addEventListener("click", () => {
   if (currentState === GameState.READY_TO_START) {
     try {
-      updateCurrentLeave();
-      if (currentLeave !== currentPosition) {
-        currentState = GameState.PLAYING;
-      } else {
-        currentState = GameState.LOST;
-      }
+      updateCurrentSkill();
+      currentState = nextStatus(currentPair, players);
       updateGameStatus();
     } catch (e) {
       alert(e.message);
     }
-  } else if (currentState === GameState.PLAYING) {
-    // Add behavior for "Next" action, like progressing the game or handling win/loss states
-    currentPosition -= 1;
-    currentPayout = payoutsAmounts[currentPosition];
-    if (currentLeave !== currentPosition) {
-      updatePositionAndPayout();
-    } else if (currentPosition === 1) {
+  } else if (currentState === GameState.ENCOUTERED) {
+    fight(
+      currentPosition,
+      currentPair,
+      players,
+      currentSkill,
+      currentPayout,
+      payoutsAmounts
+    );
+    if (players[1].position === "") {
       currentState = GameState.WON;
-      updateGameStatus();
     } else {
       currentState = GameState.LOST;
-      updateGameStatus();
     }
+    updateGameStatus();
+  } else if (currentState === GameState.PASSED) {
+    fight(
+      currentPosition,
+      currentPair,
+      players,
+      currentSkill,
+      currentPayout,
+      payoutsAmounts
+    );
+    currentState = nextStatus(currentPair, players);
+    updateGameStatus();
+  } else if (currentState === GameState.LOST) {
+    endTheGame(
+      currentPosition,
+      currentPair,
+      players,
+      currentSkill,
+      currentPayout,
+      payoutsAmounts
+    );
+    currentState = GameState.END_OF_GAME;
+    updateGameStatus();
+  } else if (currentState === GameState.WON) {
+    if (currentPosition["current"] === 1) {
+      currentState = GameState.END_OF_GAME;
+    } else {
+      currentState = nextStatus(currentPair, players);
+    }
+    updateGameStatus();
   }
 });
 
@@ -124,32 +216,18 @@ startButton.addEventListener("click", () => {
 restartButton.addEventListener("click", () => {
   currentState = GameState.READY_TO_START;
   updateGameStatus();
-  currentPosition = 100;
-  currentPayout = payoutsAmounts[currentPosition];
-  currentLeave = randBetween(1, 100);
 });
 
-function updateCurrentLeave() {
+function updateCurrentSkill() {
   if (
-    userInput.value <= 100 &&
-    userInput.value > 0 &&
-    Number.isInteger(Number(userInput.value))
+    userInput.value <= 1 &&
+    userInput.value >= 0 &&
+    !isNaN(userInput.value) &&
+    userInput.value !== ""
   ) {
-    if (skillFactor.checked) {
-      let minValue = 100;
-      for (let i = 1; i <= userInput.value; i++) {
-        minValue = Math.min(minValue, randBetween(1, 100));
-      }
-      currentLeave = minValue;
-    } else {
-      let maxValue = 1;
-      for (let i = 1; i <= userInput.value; i++) {
-        maxValue = Math.max(maxValue, randBetween(1, 100));
-      }
-      currentLeave = maxValue;
-    }
+    currentSkill = Number(userInput.value);
   } else {
-    throw new Error("Factor must be an integer between 1 and 100");
+    throw new Error("Factor must be an number between 0 and 1");
   }
 }
 
